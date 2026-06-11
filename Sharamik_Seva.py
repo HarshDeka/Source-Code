028561e5c5a2977048a8a78bdea3f70b9250124b
Harsh Deka <harshhitesh2@gmail.com>
2026-06-11 18:44:10 +0530

    cleaned

--- Sharamik_Seva.py
+++ Sharamik_Seva.py
@@ -125,8 +125,8 @@ class ShramikSevaApp(tk.Tk):
         password.bind("<Return>", on_enter)
         def login():
             user, pw = username.get(), password.get()
-            if user == "1" and pw == "1":
-                self.current_user = {"role": "client", "username": "1"}
+            if user == "" and pw == "":
+                self.current_user = {"role": "client", "username": ""}
                 self.is_employer = False
                 self.subscription_plan = None
                 self.sub_status_label.config(text="Subscription: None")
